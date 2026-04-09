6e908b4f9dd5255ef5fa9567109c49ef3f3ba622
jonasE-10 <loco.ellekjaer@gmail.com>
2026-04-09 08:59:57 +0200

    Added Classes: - PaymentController - SessionConfig - ThymeleafConfig - UserController - ShoppingCartController
    Classes in progress:
    - ThymeleafConfig
    - SessionConfig
    - UserController

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,71 @@
+package controllers;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import exceptions.DatabaseException;
+import persistence.ConnectionPool;
+import persistence.UserMapper;
+import entities.User;
+
+public class UserController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool){
+        app.get("/createuser", ctx -> ctx.render("createuser.html"));
+        app.post("/createuser", ctx -> createUser(ctx, connectionPool));
+        app.get("/login", ctx -> ctx.render("login.html"));
+        app.post("/login", ctx -> loginUser(ctx, connectionPool));
+        app.post("/logout", ctx -> logoutUser(ctx));
+    }
+
+    public static void createUser(Context ctx, ConnectionPool connectionPool){
+        //hent data user/pass
+        String username = ctx.formParam("username");
+        String password = ctx.formParam("password");
+
+        //validate inputs
+
+        //opret user i DB
+        try {
+            UserMapper.createuser(username, password, connectionPool);
+            //alert bruger om at user er blevet lavet
+            String createConfirm = username+" er nu blevet oprettet som bruger!";
+            ctx.attribute("msg", createConfirm);
+            //tilbage til forside
+            ctx.render("index.html");
+        } catch (DatabaseException e) {
+            ctx.attribute("msg", e.getMessage());
+            ctx.render("createuser.html");
+        }
+
+    }
+
+    public static void loginUser(Context ctx, ConnectionPool connectionPool){
+        // henter data fra form
+        String username = ctx.formParam("username");
+        String password = ctx.formParam("password");
+
+        //logger bruger ind
+        try {
+            User user = UserMapper.login(username, password, connectionPool);
+            // sæt session til den brugte user
+            ctx.sessionAttribute("currentUser", user);
+            //send bruger til messageboard
+            ctx.redirect("/messageboard");
+        } catch (DatabaseException e) {
+            //hvis fejl skriv på siden
+            ctx.attribute("msg", e.getMessage());
+            //send bruger tilbage til login siden
+            ctx.render("login.html");
+        }
+
+    }
+
+    public static void logoutUser(Context ctx){
+        // stop session
+        ctx.sessionAttribute("currentUser", null);
+        // alert bruger om at der er blevet logget ud
+        String logoutConfirm = "Du er nu logget ud";
+        ctx.attribute("msg", logoutConfirm);
+        ctx.render("index.html");
+    }
+}
