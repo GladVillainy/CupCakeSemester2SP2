1615b168c6a16159b55943017c38fcf93e45af6e
MikkelNorden <mikkelhoegnorden@gmail.com>
2026-04-12 23:28:23 +0200

    Topping, Bottom, and Cupcake entity classes added

---
+++ src/main/java/entities/Cupcake.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Cupcake {
+
+    private Topping topping;
+    private Bottom bottom;
+
+    public Cupcake(Topping topping, Bottom bottom) {
+        this.topping = topping;
+        this.bottom = bottom;
+    }
+}
