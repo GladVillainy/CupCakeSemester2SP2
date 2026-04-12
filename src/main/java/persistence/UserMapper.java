4833ff7eee8e13b0d310b87c355a9ffeade28cec
MikkelNorden <mikkelhoegnorden@gmail.com>
2026-04-12 13:10:31 +0200

    UserMapper class done (for now)

--- src/main/java/persistence/UserMapper.java
+++ src/main/java/persistence/UserMapper.java
@@ -48,7 +48,6 @@ public class UserMapper {
             ps.setInt(1, id);
             try(ResultSet rs = ps.executeQuery()) {
                 if(rs.next()) {
-                    id = rs.getInt("user_id");
                     String email = rs.getString("email");
                     String password = rs.getString("password");
                     String role = rs.getString("role");
@@ -60,5 +59,54 @@ public class UserMapper {
             throw new DatabaseException("DB fejl: " + e.getMessage());
         }
         return u;
+    }
+
+    public static User login(String email, String password, ConnectionPool connectionPool) {
+
+        String sql = "SELECT * FROM public.users WHERE email = ? AND password = ?";
+
+        try(Connection connection = connectionPool.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                int id = rs.getInt("user_id");
+                String role = rs.getString("role");
+                double balance = rs.getDouble("balance");
+                return new User(id, email, password, role, balance);
+            } else {
+                throw new DatabaseException("Fejl i login. Prøv igen");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("DB fejl: " + e.getMessage());
+        }
+    }
+
+    public static void createUser(String email, String password, String role, double balance, ConnectionPool connectionPool) {
+
+        String sql = "INSERT INTO public.users (email, password, role, balance) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ps.setString(3, role);
+            ps.setDouble(4, balance);
+
+            int rowsAffected = ps.executeUpdate();
+            if(rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved oprettelse af ny bruger");
+            }
+        } catch (SQLException e) {
+            String msg = "Der er sket en fejl. Prøv igen";
+            if (e.getMessage().startsWith("ERROR: duplicate key value ")) {
+                msg = "Brugernavnet findes allerede. Vælg et andet";
+            }
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 }
